87c355c77cbaff48542efe9c0e771caa2f81cad3
Eric Liu <eric.liu.developer@gmail.com>
2022-05-25 16:11:38 -0700

    tests deploys contract

--- test/Play.test.js
+++ test/Play.test.js
@@ -2,30 +2,50 @@ const assert = require('assert');
 const ganache = require('ganache');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+const { abi, evm} = require('../compile');
 
+// Simple example
+// class Car {
+//     park(){
+//         return 'stopped';
+//     }
 
-class Car {
-    park(){
-        return 'stopped';
-    }
+//     drive(){
+//         return 'vroom';
+//     }
+// }
 
-    drive(){
-        return 'vroom';
-    }
-}
+// let car;
 
-let car;
+// beforeEach(() => {
+//     car = new Car();
+// });
 
-beforeEach(() => {
-    car = new Car();
+// describe('Car', () => {
+//     it('can park', () => {
+//         assert.equal(car.park(), 'stopped');
+//     });
+
+//     it('can drive', () => {
+//         assert.equal(car.drive(), 'vroom');
+//     });
+// });
+
+let accounts;
+let inbox;
+// Test Ethereum
+beforeEach(async () => {
+    // Get a list of accounts.
+    accounts = await web3.eth.getAccounts();
+
+    // Use an account to deploy contracts
+   inbox = await new web3.eth.Contract(abi)
+    .deploy({data: evm.bytecode.object, arguments: ['Hello Wold!']})
+    .send({ from: accounts[0], gas: '1000000'} )
 });
 
-describe('Car', () => {
-    it('can park', () => {
-        assert.equal(car.park(), 'stopped');
-    });
-
-    it('can drive', () => {
-        assert.equal(car.drive(), 'vroom');
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        console.log(inbox);
     });
 });
