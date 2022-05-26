1f0cfc91cf625c261848db4cdf62e5aa213fd670
Eric Liu <eric.liu.developer@gmail.com>
2022-05-25 20:43:54 -0700

    more tests

--- test/Play.test.js
+++ test/Play.test.js
@@ -33,6 +33,8 @@ const { abi, evm} = require('../compile');
 
 let accounts;
 let inbox;
+const INITIAL_MSG = 'Hello World!';
+
 // Test Ethereum
 beforeEach(async () => {
     // Get a list of accounts.
@@ -40,12 +42,19 @@ beforeEach(async () => {
 
     // Use an account to deploy contracts
    inbox = await new web3.eth.Contract(abi)
-    .deploy({data: evm.bytecode.object, arguments: ['Hello Wold!']})
+    .deploy({data: evm.bytecode.object, arguments: [INITIAL_MSG]})
     .send({ from: accounts[0], gas: '1000000'} )
 });
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(inbox);
+        // console.log(inbox);
+        assert.ok(inbox.options.address);
+    });
+
+    it('has an initial message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(INITIAL_MSG, message);
+        console.log(message);
     });
 });
